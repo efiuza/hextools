e54cba6b26ac27173c790e176d190c1969e156aa
Emanuel Oliveira <efiuza@me.com>
2013-12-02 17:50:08 -0200

    Added some useful macros...

--- hexl.c
+++ hexl.c
@@ -4,24 +4,35 @@
  * Local Constants and Macros
  */
 
-#define IS_EOL_CHAR(a) \
-    ((a) == '\n' || (a) == '\r')
+#define COMMENT_CHAR '#'
 
-#define IS_COMMENT_CHAR(a) \
-    ((a) == '#')
+#define IS_EOL_CHAR(ch) \
+    ((ch) == '\n' || (ch) == '\r')
 
-#define IS_BLANK(a) \
-    ((a) == ' '     \
-    || (a) == '\t'  \
-    || (a) == '\n'  \
-    || (a) == '\v'  \
-    || (a) == '\f'  \
-    || (a) == '\r')
+#define IS_COMMENT_CHAR(ch) \
+    ((ch) == COMMENT_CHAR)
 
-#define IS_HEXDIGT(a)              \
-    (((a) >= '0' && (a) <= '9')    \
-    || ((a) >= 'A' && (a) <= 'F')  \
-    || ((a) >= 'a' && (a) <= 'f'))
+#define IS_BLANK_CHAR(ch) \
+    ((ch) == ' '          \
+    || (ch) == '\t'       \
+    || (ch) == '\n'       \
+    || (ch) == '\v'       \
+    || (ch) == '\f'       \
+    || (ch) == '\r')
+
+#define IS_HEXDIGT_CHAR(ch)          \
+    (((ch) >= '0' && (ch) <= '9')    \
+    || ((ch) >= 'A' && (ch) <= 'F')  \
+    || ((ch) >= 'a' && (ch) <= 'f'))
+
+#define HEX2BIN(ch)                 \
+    ((ch) >= '0' && (ch) <= '9'     \
+    ? (ch) - '0'                    \
+    : ((ch) >= 'A' && (ch) <= 'F'   \
+      ? (ch) - 'A' + 10             \
+      : ((ch) >= 'a' && (ch) <= 'f' \
+        ? (ch) - 'a' + 10           \
+        : 0)));
 
 /*
  * Private Routines
@@ -35,33 +46,35 @@
 
 int hexl_encode(int cnt, const char *src, char *dst) {
 
-    char ch, buf[2];
+    char ch, buf;
     int i, is_cmmt;
-
-    if (cnt < 1)
-        goto _abort;
 
     for (i = 0; i < cnt; i++) {
         ch = *(src + i);
-        /* checks whether the current character
-           belongs to a comment... */
+        /* Checks whether the current character
+           belongs to a comment section... */
         if (is_cmmt) {
             if (IS_EOL_CHAR(ch))
                 is_cmmt = 0;
+            /* On windows, line endings are marked
+               with two characteres (CR + LF). The
+               second one can be ignored once it is
+               also considered a blank character. */
             continue;
         }
         if (IS_COMMENT_CHAR(ch)) {
             is_cmmt = 1;
             continue;
         }
-        if (IS_BLANK(ch))
+        if (IS_BLANK_CHAR(ch))
             continue;
+        if (IS_HEXDIGIT_CHAR(ch)) {
+            buf = HEX2BIN(ch) << 4;
+        }
+        break;
     }
 
     return i;
-
-_abort:
-    return -1;
 
 }
 
