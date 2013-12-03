9a66c63606eeeb0e92a6a59ee08a20d5373a084a
Emanuel Oliveira <efiuza@me.com>
2013-12-03 18:11:41 -0200

    Added main program file...

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hexl.h"
+
+#define BUFFER_SIZE 100
+
+int main(int argc, char *argv[]) {
+
+    FILE *ifp, *ofp;
+    char ifn[FILENAME_MAX], ofn[FILENAME_MAX];
+    char ibuf[BUFFER_SIZE], obuf[BUFFER_SIZE];
+    int cnt, rd, wrt, st;
+
+    /* Parse applications arguments... */
+    /* ... */
+
+    /* Open input file... */
+    ifp = fopen(ifn, "rb");
+    if (ifp == NULL) {
+        printf("Error opening input file: \"%s\"\n", ifn);
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+
+}
+
