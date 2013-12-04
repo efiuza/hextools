b6ede7e17defdb7cf4ae32feb6024bc0f0847514
Emanuel Oliveira <efiuza@me.com>
2013-12-04 09:05:17 -0200

    Corrections additions to main program...

--- main.c
+++ main.c
@@ -5,6 +5,18 @@
 
 #define BUFFER_SIZE 100
 
+/* Error Messages */
+
+#define ERROR_OPENING_INPUT \
+    "Error opening input file: \"%s\"\n"
+
+#define ERROR_OPENING_OUTPUT \
+   "Error opening output file: \"%s\"\n" 
+
+/*
+ * Main Program Entry Point
+ */
+
 int main(int argc, char *argv[]) {
 
     FILE *ifp, *ofp;
@@ -12,25 +24,34 @@ int main(int argc, char *argv[]) {
     char ibuf[BUFFER_SIZE], obuf[BUFFER_SIZE];
     int cnt, rd, wrt, st;
 
-    /* Parse applications arguments... */
+    /* Parse application arguments... */
     /* ... */
 
     /* Open input file... */
     ifp = fopen(ifn, "rb");
     if (ifp == NULL) {
-        printf("Error opening input file: \"%s\"\n", ifn);
-        return EXIT_FAILURE;
+        printf(ERROR_OPENING_INPUT, ifn);
+        goto abort_dft;
     }
 
     /* Open output file... */
     ofp = fopen(ofn, "wb");
     if (ofp == NULL) {
-        printf("Error openig output file: \"%s\"\n", ofn);
-        fclose(ifp);
-        return EXIT_FAILURE;
+        printf(ERROR_OPENING_OUTPUT, ofn);
+        goto abort_alt1;
     }
 
+success:
     return EXIT_SUCCESS;
+
+abort_alt2:
+    fclose(ofp);
+
+abort_alt1:
+    fclose(ifp);
+
+abort_dft:
+    return EXIT_FAILURE;
 
 }
 
