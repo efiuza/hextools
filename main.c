e500e8195a3c99b521a35aaec41a86724b940c4c
Emanuel Oliveira <efiuza@me.com>
2013-12-09 12:41:26 -0200

    Started testing hexl_encode usage from main...

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include "hexl.h"
 
-#define BUFFER_SIZE 100
+#define BUFFER_SIZE 50
 
 /* Error Messages */
 
@@ -25,6 +25,12 @@
 #define ERROR_PROCESSING_INPUT \
     "Error processing input file (#%04ld)...\n"
 
+#define ERROR_BUFFER_NARROW \
+    "Sorry, insufficient read buffer...\n"
+
+#define ERROR_UNKNOWN \
+    "Sorry, an unknown error has just happened...\n"
+
 /*
  * Main Program Entry Point
  */
@@ -34,11 +40,17 @@ int main(int argc, char *argv[]) {
     FILE *ifp, *ofp;
     char ifn[FILENAME_MAX], ofn[FILENAME_MAX];
     char ibuf[BUFFER_SIZE], obuf[BUFFER_SIZE];
-    int eno, off, rt, rd, wr, cnt, frd, fwr;
+    int eno, diff, rt, rd, wr, cnt, frd, fwr;
     size_t lrd, lwr, lcnt, lpos;
 
     /* Parse application arguments... */
     /* ... */
+    if (argc < 3) {
+        fputs(ERROR_BAD_USAGE, stderr);
+        goto abort_dft;
+    }
+    strcpy(ifn, argv[1]);
+    strcpy(ofn, argv[2]);
 
     /* Open input file... */
     ifp = fopen(ifn, "rb");
@@ -54,28 +66,40 @@ int main(int argc, char *argv[]) {
         goto abort_alt1;
     }
 
-    off = 0;
+    diff = 0;
+    lpos = 0;
     lcnt = BUFFER_SIZE;
 
-    while ((lrd = fread((void *)(ibuf + off), 1, lcnt, ifp)) > 0) {
-        if (lrd < lcnt && (eno = ferror(ifp))) {
+    while ((lrd = fread(ibuf + diff, 1, lcnt - diff, ifp)) > 0) {
+        if (lrd < lcnt - diff && (eno = ferror(ifp))) {
             fprintf(stderr, ERROR_READING_INPUT, eno);
             goto abort_alt2;
         }
-        cnt = off + (int)lrd;
+        cnt = diff + (int)lrd;
         rt = hexl_encode(cnt, ibuf, obuf, &rd, &wr);
+        lpos += rd;
+
+        /* Update diff (difference between what
+           has been supplied and what has actually
+           been read) and move remaining content
+           to the begining of buffer... */
+        diff = cnt - rd;
+        if (rd > 0 && diff > 0)
+            memmove(ibuf, ibuf + rd, diff);
+
         if (rt == HEXL_EILSEQ || rt == HEXL_EINVAL) {
             fprintf(stderr, ERROR_PROCESSING_INPUT, lpos);
             goto abort_alt2;
         } else if (rt == HEXL_ENOBUFS) {
-            off = cnt - rd;
-            if (off > 0) {
-                memmove(ibuf, ibuf + rd, off);
+            if (rd < 1) {
+                fputs(ERROR_BUFFER_NARROW, stderr);
+                goto abort_alt2;
             }
         } else if (rt != HEXL_OK) {
             fputs(ERROR_UNKNOWN, stderr);
             goto abort_alt2;
         }
+
         if (wr > 0) {
             if (fwrite(obuf, 1, wr, ofp) != wr) {
                 eno = ferror(ofp);
